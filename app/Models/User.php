ac0899afcbd08b9498daeda1c780c29a2df53a40
Dedicker Benoit <dedickerbenoit@gmail.com>
2026-02-05 22:40:43 +0100

    chore(auth): configure Laravel Passport
    - Add Passport package and migrations
    - Configure API guard with Passport driver
    - Add HasApiTokens trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,22 +6,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * The attributes that are NOT mass assignable.
+     * Using $guarded to prevent mass assignment vulnerabilities.
+     * @var array<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
